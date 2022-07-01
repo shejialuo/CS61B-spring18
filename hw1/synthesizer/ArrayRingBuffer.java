9808d8a45a2cd0d5578dc15fb60a848e7c35b2d7
shejialuo <shejialuo@gmail.com>
2022-07-01 22:26:17 +0800

    Finish Homework 1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,90 @@
+package synthesizer;
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+        rb = (T[]) new Object[capacity];
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last = (last + 1) % capacity;
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T num = rb[first];
+        first = (first + 1) % capacity;
+        fillCount--;
+        return num;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
+    }
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        private int pos;
+        private int curNum;
+        public ArrayRingBufferIterator() {
+            pos = first;
+            curNum = 0;
+        }
+
+        public boolean hasNext() {
+            return curNum < fillCount;
+        }
+
+        public T next() {
+            T retValue = rb[pos];
+            pos = (pos + 1) % capacity;
+            curNum++;
+            return retValue;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -8,8 +8,58 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void initializationTest() {
+        ArrayRingBuffer<Integer> arrayRingBuffer = new  ArrayRingBuffer<Integer>(10);
+        assertTrue(arrayRingBuffer.isEmpty());
+        assertFalse(arrayRingBuffer.isFull());
+        assertEquals(10, arrayRingBuffer.capacity());
+    }
+
+    @Test
+    public void basicFunctionalityTest() {
+        ArrayRingBuffer<Integer> arrayRingBuffer = new  ArrayRingBuffer<Integer>(10);
+
+        arrayRingBuffer.enqueue(5);
+        assertFalse(arrayRingBuffer.isEmpty());
+        int num = arrayRingBuffer.peek();
+        assertEquals(5, num);
+
+        for (int i = 0; i < 9; i++) {
+            arrayRingBuffer.enqueue(i);
+        }
+
+        assertTrue(arrayRingBuffer.isFull());
+        num = arrayRingBuffer.dequeue();
+        assertEquals(5, num);
+        for (int i = 0; i < 9; i++) {
+            num = arrayRingBuffer.dequeue();
+            assertEquals(i, num);
+        }
+        assertTrue(arrayRingBuffer.isEmpty());
+    }
+
+    @Test
+    public void cornerTest() {
+        ArrayRingBuffer<Integer> arrayRingBuffer = new  ArrayRingBuffer<Integer>(2);
+        arrayRingBuffer.enqueue(5);
+        arrayRingBuffer.enqueue(4);
+        int num = arrayRingBuffer.dequeue();
+        assertEquals(5, num);
+
+        arrayRingBuffer.enqueue(3);
+        num = arrayRingBuffer.peek();
+        assertEquals(4, num);
+
+        arrayRingBuffer.dequeue();
+        num = arrayRingBuffer.peek();
+        assertEquals(3, num);
+
+        assertEquals(1, arrayRingBuffer.fillCount());
+        arrayRingBuffer.enqueue(101);
+
+        arrayRingBuffer.dequeue();
+        num = arrayRingBuffer.peek();
+        assertEquals(101, num);
     }
 
     /** Calls tests for ArrayRingBuffer. */
